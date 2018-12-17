713f90b114db6e262a9efa1d00b1e6777320adcd
fabricio zanette <fabzanette@gmail.com>
2018-12-17 09:32:19 -0200

    added destroy action for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,5 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:show, :edit, :update]
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
   def index
     @portfolio_items = Portfolio.all
   end
@@ -33,6 +33,13 @@ class PortfoliosController < ApplicationController
       else
         format.html { render :new }
       end
+    end
+  end
+
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully destroyed.' }
     end
   end
 
