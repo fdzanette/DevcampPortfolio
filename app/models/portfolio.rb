03fbba7665881562615a0b651ef2c5f7954b900a
fabricio zanette <fabzanette@gmail.com>
2018-12-18 14:58:23 -0200

    implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,12 @@ class Portfolio < ApplicationRecord
   validates :body, presence: true
   validates :main_image, presence: true
   validates :thumb_image, presence: true
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+
+  end
 end
