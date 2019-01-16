2c5a6214c4a4b8faefdc66f2224da580405ecbd2
fabricio zanette <fabzanette@gmail.com>
2019-01-16 15:15:13 -0200

    Implemented cloudinary upload service. fixed img size for porfolios index and show

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'petergate', '~> 2.0', '>= 2.0.1'
 gem 'font-awesome-rails', '~> 4.7', '>= 4.7.0.4'
 gem 'kaminari', '~> 1.1', '>= 1.1.1'
 gem 'jquery-ui-rails', '~> 6.0', '>= 6.0.1'
+gem 'cloudinary', '~> 1.11', '>= 1.11.1'
 gem 'carrierwave', '~> 1.3', '>= 1.3.1'
 gem 'dotenv-rails', groups: [:development, :test]
 
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,21 +1,12 @@
 class Portfolio < ApplicationRecord
   has_many :technologies, dependent: :destroy
   accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank? }
-  mount_uploader :main_image, PhotoUploader
-  mount_uploader :thumb_image, PhotoUploader
-  include Placeholder
 
   validates :title, presence: true
   validates :body, presence: true
-  validates :main_image, presence: true
-  validates :thumb_image, presence: true
 
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
-    self.thumb_image ||= Placeholder.image_generator(height: "350", width: "200")
-  end
+  mount_uploader :main_image, PortfolioUploader
+  mount_uploader :thumb_image, PortfolioUploader
 
   def self.by_position
     order("position ASC")
